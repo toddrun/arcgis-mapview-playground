45e8d6276ee12e32779a39ad338413bb97ead520
Todd Runstein <toddrun@gmail.com>
2024-09-10 10:21:54 -0700

    Reorder routes and startup logic

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-app.listen(8080, () => {
-  console.log('server listening on port 8080')
-});
-
 app.use(cors());
+
+app.get('/oauth/arcgis/callback', (req, res) => {
+  res.sendFile(path.join(__dirname, './oauth-callback.html'));
+});
 
 app.get('/', express.static(path.join(__dirname, 'client/build')));
 
-app.get('/oauth/arcgis/callback', (req, res) => {
-  res.sendFile(path.join(__dirname, 'oauth-callback.html'));
+app.listen(8080, () => {
+  console.log('server listening on port 8080')
 });
