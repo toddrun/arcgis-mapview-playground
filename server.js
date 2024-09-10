f2196d9eef46ea79a973558fcd0d38e3ec85fd8b
Todd Runstein <toddrun@gmail.com>
2024-09-10 09:36:11 -0700

    Try to eliminate cors as a blocker

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 app.listen(8080, () => {
   console.log('server listening on port 8080')
 });
+
+app.use(cors());
 
 app.get('/', express.static(path.join(__dirname, 'client/build')));
 
