c95beb6e69ba4f70710ab324f7deff064c561079
Todd Runstein <toddrun@gmail.com>
2024-09-10 08:02:30 -0700

    WIP - gotta have a server to handle the callback

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(8080, () => {
+  console.log('server listening on port 8080')
+});
+
+app.get('/', express.static(path.join(__dirname, 'client/build')));
+
+app.get('/oauth/arcgis/callback', (req, res) => {
+  res.send("<html></html>")
+});
