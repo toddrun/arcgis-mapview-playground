e4e326277d09a0025f1d1684e72cd1eaf4c4876b
Todd Runstein <toddrun@gmail.com>
2024-09-13 14:16:45 -0700

    Remove extra interactions with the mapview

--- src/main-view.tsx
+++ src/main-view.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ArcgisMapview from './map/arcgis-mapview';
-import OnExtentsChange from './plugins/on-extents-change';
 import { Bounds } from './map/arcgis-types';
-import renderLayers from './plugins/render-layers';
 import { LayerSetting } from './arcgis-layer-loader';
 
 interface MainViewState {
@@ -18,32 +16,7 @@ interface Props {
 }
 
 const MainView: React.FC<Props> = ({loadedLayers}) => {
-  // console.log('rendering mainview...')
-
-  const [state, setState] = useState<MainViewState | undefined>(undefined);
-
-  const refresh = () => {
-    // console.log('@@: main-view | refresh just called')
-    setState({...state, requestUpdateLayers: true})
-  }
-
-  const handleMapExtentsChange = (center, zoom, bounds) => {
-    // console.log('@@: plugin just called: ', center, zoom, bounds.getSouthWest(), bounds.getNorthEast())
-    setState({ ...state, center, zoom, bounds})
-    refresh();
-  }
-
-  const plugins = [
-    OnExtentsChange(handleMapExtentsChange),
-    renderLayers(() => {
-      // console.log('@ !!! renderLayers |  calling shouldrefresh');
-      const shouldRefresh = state?.requestUpdateLayers ?? false;
-      if (shouldRefresh) {
-        setState({  ...state, requestUpdateLayers: false });
-      }
-      return shouldRefresh;
-    },)
-  ]
+  const plugins = []
   return (
     <main className="main-view">
       <ArcgisMapview plugins={plugins} loadedLayers={loadedLayers} />
